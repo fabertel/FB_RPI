0122973ac5f5b1d83fb11cdb96b859374c2b7c20
Fabio B <fabio.bertellotti@gmail.com>
2017-10-01 16:30:47 +0200

    Update FB106_send_emails.py

--- FB106_send_emails.py
+++ FB106_send_emails.py
@@ -1,22 +1,16 @@
-# versione facile ma funziona .. provare commando EMAIL 
+
 import smtplib
 
-SUBJECT = 'titolo email2'
-TEXT = 'tuerytyuklhoklk'
-msg = 'Subject: %s\n\n%s' % (SUBJECT, TEXT)
+def EEMAIL(SUBJECT,TEXT):
+    msg = 'Subject: %s\n\n%s' % (SUBJECT, TEXT)
+    fromaddr = 'fabio.bertellotti@gmail.com'
+    toaddrs  = 'fabio.bertellotti@gmail.com'
+    username = 'fabio.bertellotti@gmail.com'
+    password = 'cinghiale3'
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.starttls()
+    server.login(username,password)
+    server.sendmail(fromaddr, toaddrs, msg)
+    server.quit()
 
-fromaddr = 'fabio.bertellotti@gmail.com'
-toaddrs  = 'fabio.bertellotti@gmail.com'
-
-
-# Credentials (if needed)
-# username = 'fabio.bertellotti@gmail.com'
-# password = 'cinghiale3'
-
-# The actual mail send
-server = smtplib.SMTP('smtp.gmail.com:587')
-server.starttls()
-server.login(username,password)
-server.sendmail(fromaddr, toaddrs, msg)
-server.quit()
-
+EEMAIL('a','b')
